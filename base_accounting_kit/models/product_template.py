83f6b405200b9cca6c29792e07c7f94b93488027
Tonny <corner.store59@gmail.com>
2024-03-07 19:48:59 -0600

    [ADD] Se agrega el campo de is_asset al producto template

--- base_accounting_kit/models/product_template.py
+++ base_accounting_kit/models/product_template.py
@@ -25,6 +25,7 @@ from odoo import api, fields, models
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
+    is_asset = fields.Boolean(string='Is Asset', company_dependent=True)
     asset_category_id = fields.Many2one(
         'account.asset.category', string='Asset Type', company_dependent=True,
         ondelete="restrict")
